1223ac82cedd043d20f60af46eb5bbf7cdcc7d09
neo.alienson <neo@padblish.com>
2020-09-10 08:18:08 -0700

    Update pihole.sh

--- pc/pihole.sh
+++ pc/pihole.sh
@@ -1,4 +1,5 @@
-# 
+# --shm-size="5g" is added to prevent it from keeping restart
+# enable the firewall rules on Windows
 docker run -d --name pihole -e ServerIP=192.16.18.200 \
 -e WEBPASSWORD=password -e TZ=Asia/Hong_Kong -e DNS1=192.168.18.1 -e DNS2=1.1.1.1 -e DNS3=1.0.0.1 -p 80:80 -p 53:53/tcp -p 53:53/udp -p 443:443 \
 --restart=unless-stopped --shm-size="5g" pihole/pihole:latest
