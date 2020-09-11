78a92cdf6fac3c1a9acdfff8ab99d020a3c72a17
Neo Alienson <neo@padblish.com>
2020-09-12 02:42:40 +0800

    update

--- pc/home_assistant.sh
+++ pc/home_assistant.sh
@@ -1,1 +1,4 @@
-docker run --init -d --name="home-assistant" -e "TZ=Asia/Hong_Kong" -v "/home/neo/nexus/pc:/config" -p 8123:8123 homeassistant/home-assistant:stable
+docker run --init -d --name="home-assistant" -e "TZ=Asia/Hong_Kong" \
+-v "/home/neo/nexus/pc/hassio:/config" \
+--restart=unless-stopped \
+-p 8123:8123 homeassistant/home-assistant:stable
