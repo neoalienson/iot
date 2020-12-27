a969e192416a8f72885c8c5d1d0adf21da4eeffd
Neo Alienson <neo@01man.com>
2020-12-27 23:33:07 +0800

    upgrade

--- pc/home_assistant.sh
+++ pc/home_assistant.sh
@@ -4,6 +4,5 @@ docker run --init -d --name="home-assistant" -e "TZ=Asia/Hong_Kong" \
 -p 8123:8123/tcp \
 -p 5353:5353/udp \
 -p 51827:51827 \
-homeassistant/home-assistant:0.118.0.dev20201115
-#homeassistant/home-assistant:stable
-#homeassistant/home-assistant:0.114.4
+homeassistant/home-assistant:stable
+#homeassistant/home-assistant:0.118.0.dev20201115
