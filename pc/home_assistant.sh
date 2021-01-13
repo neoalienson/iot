73807cb7497bb1dc487d27ad73af803cc10ef023
Neo Alienson <neo@01man.com>
2021-01-13 23:15:29 +0800

    remove unsed ports as the recent bug from microsoft update

--- pc/home_assistant.sh
+++ pc/home_assistant.sh
@@ -2,7 +2,7 @@ docker run --init -d --name="home-assistant" -e "TZ=Asia/Hong_Kong" \
 -v "/home/neo/nexus/pc/hassio:/config" \
 --restart=unless-stopped \
 -p 8123:8123/tcp \
--p 5353:5353/udp \
--p 51827:51827 \
 homeassistant/home-assistant:stable
 #homeassistant/home-assistant:0.118.0.dev20201115
+#-p 5353:5353/udp \
+#-p 51827:51827 \
